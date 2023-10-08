a168ac31105216e9bdc658e92ff6db8b86d6a3d0
Ahmed Chakib <schackib@gmail.com>
2023-10-08 15:36:35 +0100

    Add logs with Logback

--- src/main/java/com/cronos/ahmed/mapper/UserMapper.java
+++ src/main/java/com/cronos/ahmed/mapper/UserMapper.java
@@ -7,14 +7,9 @@ import org.springframework.beans.BeanUtils;
 
 public class UserMapper {
     private UserMapper(){}
-    public static User fromDtoToUser(UserDto userDto){
-            User user = new User();
-            BeanUtils.copyProperties(userDto, user);
-            return user;
-    }
-    public static UserDto fromUserToDto(User user){
-        UserDto userDto = new UserDto();
-        BeanUtils.copyProperties(user, userDto);
-        return userDto;
+    public static User fromDtoToUser(UserDto userDto) {
+        User user = new User();
+        BeanUtils.copyProperties(userDto, user);
+        return user;
     }
 }
--- src/main/java/com/cronos/ahmed/web/UserController.java
+++ src/main/java/com/cronos/ahmed/web/UserController.java
@@ -9,14 +9,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    Logger logger = LoggerFactory.getLogger(UserController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
