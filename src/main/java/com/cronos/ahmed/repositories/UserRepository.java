0aa1d29dd336c886c40c64ed770141daa57c8055
Ahmed Chakib <78487960+chackib@users.noreply.github.com>
2024-01-02 17:05:44 +0100

    Delete src/main/java/com/cronos/ahmed/repositories directory

--- src/main/java/com/cronos/ahmed/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cronos.ahmed.repositories;
-
-import com.cronos.ahmed.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-}
