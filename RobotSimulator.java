a4b365fd3797bc63e82e4e01ab51a8929dfdf7b1
Ahmed Chakib <78487960+chackib@users.noreply.github.com>
2024-01-02 17:11:24 +0100

    Create RobotSimulator.java

--- RobotSimulator.java
+++ RobotSimulator.java
@@ -1,1 +1,22 @@
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            Robot r = new Robot();
+            int i = 0;
+            
+            while (i < 3000) {
+                r.keyPress(KeyEvent.VK_R);
+                r.keyRelease(KeyEvent.VK_R);
+                i++;
+                System.out.println(i);
+                Thread.sleep(10000);
+            }
+        } catch (AWTException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
